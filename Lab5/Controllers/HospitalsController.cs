b612b74280858747fc8456ec278b26fa476d2491
RuDomitori <RuDomitori@gmail.com>
2021-04-17 19:29:47 +0700

    Doctor CRUD

---
+++ Lab5/Controllers/HospitalsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lab5.Models;
+using Lab5.ViewModels.Hospitals;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Controllers
+{
+    public class HospitalsController : Controller
+    {
+        private ApplicationDbContext _db;
+        public HospitalsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _db.Hospitals.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(HospitalCreatingViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Hospitals.Add(new Hospital()
+                {
+                    Name = model.Name,
+                    Address = model.Address,
+                    Phones = model.Phones
+                });
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            return View();
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab5/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; } 
+
+        public Gender Gender { get; set; }
+
+    }
+}
